1b8aa80042babf672392e75be6696c3470ef9485
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-21 16:12:33 +0200

    Bunp deps

--- Carthage/Checkouts/UITestSugar/src/ui/tabbar/TabBarHelper.swift
+++ Carthage/Checkouts/UITestSugar/src/ui/tabbar/TabBarHelper.swift
@@ -1,13 +1,23 @@
 import XCTest
 
-//public enum TabButtonType: String { case homeButton, exploreButton, searchButton, myCollectionButton }
-
 public class TabBarHelper {
-    ///
-    /// Returns tabbar buttons
-    /// ## Examples: "homeButton", "exploreButton", "searchButton", "myCollectionButton"
-    ///
-    public static func tabButton(tabButtonName: String) -> XCUIElement {
-        return XCUIApplication().descendants(matching: .any).matching(identifier: tabButtonName).element
-    }
+   ///
+   /// Returns tabbar buttons based on name of the tabbar.item
+   /// ## Examples:
+   /// TabBarHelper.tabButton(tabButtonName: "homeButton").tap()
+   /// - Note: It could be useful to store names of tabbar ids in an enum like: TabButtonType: String { case homeButton, exploreButton, searchButton, myCollectionButton }
+   ///
+   public static func tabButton(tabButtonName: String) -> XCUIElement {
+      return XCUIApplication().descendants(matching: .any).matching(identifier: tabButtonName).element
+   }
+   ///
+   /// Returns tabbar buttons based on index
+   /// ## Examples:
+   /// TabBarHelper.tabButton(tabButtonName: 0).tap()
+   ///
+   public static func tabButton(idx: Int) -> XCUIElement {
+      let tabBarsQuery = XCUIApplication().tabBars
+      let uiElement = tabBarsQuery.buttons.element(boundBy: idx)
+      return uiElement
+   }
 }
