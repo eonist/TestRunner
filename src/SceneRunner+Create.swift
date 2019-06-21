a2ac2de43e583d9f24c5ef5cdbdbee051b343654
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-21 17:39:40 +0200

    Make createXCUIApp open

--- src/SceneRunner+Create.swift
+++ src/SceneRunner+Create.swift
@@ -2,9 +2,10 @@ import Foundation
 import XCTest
 
 extension SceneRunner {
-   @objc public func createXCUIApp() -> XCUIApplication{
+   @objc open func createXCUIApp() -> XCUIApplication{
       let app = XCUIApplication()
       app.launch()
       return app
    }
 }
+
