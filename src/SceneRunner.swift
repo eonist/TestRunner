3d5664037440c04f30c9271ee21c838bd3208563
eonist <30n1st@gmail.com>
2021-06-27 16:46:00 +0200

    Minor cleanup

--- src/SceneRunner.swift
+++ src/SceneRunner.swift
@@ -1,5 +1,5 @@
 import Foundation
-import IteratorIOS
+import Iterator
 import XCTest
 /**
  * - Abstract: SceneRunner plays through scenes like a playlist
@@ -9,7 +9,7 @@ import XCTest
 open class SceneRunner: ArrayIterator<SceneKind.Type>, SceneRunnerKind {
    public lazy var app: XCUIApplication = createXCUIApp() // Convenient accessor to the app
    public let user: User // Stores userName and password
-   public var scenes: [SceneKind.Type] { return collection } // stores the scenes in the sequence
+   public var scenes: [SceneKind.Type] { collection } // stores the scenes in the sequence
    public var complete: Completed // A callback that notifies the user when the sequence has completed
    /**
     * Initiate the scene-runner
--- src/protocol/SceneKind+Getter.swift
+++ src/protocol/SceneKind+Getter.swift
@@ -2,6 +2,6 @@ import Foundation
 import XCTest
 
 extension SceneKind {
-   public var app: XCUIApplication { return sceneRunner.app }
-   public var user: SceneRunner.User { return sceneRunner.user }
+   public var app: XCUIApplication { sceneRunner.app }
+   public var user: SceneRunner.User { sceneRunner.user }
 }
